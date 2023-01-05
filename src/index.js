0c70f4b054a08757d2436dee899ab9846e72125c
IPL87 <gmax_lg@ukr.net>
2023-01-05 18:19:40 +0300

    fix

---
+++ src/index.js
@@ -0,0 +1,3 @@
+// TODO: write your code in app.js
+import './js/app.js';
+import './css/style.css';
---
+++ src/js/app.js
@@ -0,0 +1,4 @@
+// TODO: write your code here
+import showHealth from './health';
+
+showHealth(70);
