dc046357fe2347bb9076d2f40f358c54f4dcd8e4
IPL87 <gmax_lg@ukr.net>
2023-01-06 12:00:54 +0300

    fix

--- src/js/__tests__/matchers.test.js
+++ src/js/__tests__/matchers.test.js
@@ -1,20 +1,20 @@
 import health from '../matchers';
 
-const test = [
+const testing = [
   { name: 'мечник', health: 10 },
   { name: 'маг', health: 100 },
   { name: 'лучник', health: 80 },
 ];
 
-const handler = test.each(test);
+const handler = test.each(testing);
 
 handler('sort by health', () => {
-  const sort = [
+  const sorted = [
     { name: 'маг', health: 100 },
     { name: 'лучник', health: 80 },
     { name: 'мечник', health: 10 },
   ];
 
-  const result = health(test);
-  expect(result).toEqual(sort);
+  const result = health(testing);
+  expect(result).toEqual(sorted);
 });
